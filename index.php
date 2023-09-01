c23b3b3241128e5631debbd433a0c80859d4c181
Adalberto Kubota <adalbertosk@gmail.com>
2023-09-01 18:01:06 +0100

    Commit 01.09.2023

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-    mysqli_connect("localhost", "adalberto", "password", "project1");
+    $link = mysqli_connect("localhost", "adalberto", "password", "project1");
 
-    if(!mysqli_connect_error()) {
-        echo "Database connection successful!";
+    if(mysqli_connect_error()) {
+        die("There was an error connecting to the database.");
     }
-    else {
-        echo "There was an error connecting to the database!";
+
+    $query = "SELECT * FROM person";
+
+    if(mysqli_query($link, $query)) {
+        echo "Query was successful!";
     }
 ?>
